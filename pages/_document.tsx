3d609617ff8409796804c82254d7f522534a970b
Mahmoud Mousa <Mahmoud.m@covergenius.com>
2022-09-06 19:48:47 +1000

    Init commit and add chakra-ui

---
+++ pages/_document.tsx
@@ -0,0 +1,34 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Html,
+  Head,
+  Main,
+  NextScript
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript initialColorMode="dark" />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
